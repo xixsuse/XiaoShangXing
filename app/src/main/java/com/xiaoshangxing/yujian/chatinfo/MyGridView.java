0e34ea2093d8e60d4ac01fd7b2d133c32fd998df
tianyang998 <1582869895@qq.com>
2016-08-19 15:32:01 +0800

    2016 8 19 遇见更新1

---
+++ app/src/main/java/com/xiaoshangxing/yujian/chatinfo/MyGridView.java
@@ -0,0 +1,30 @@
+package com.xiaoshangxing.yujian.chatInfo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * Created by 15828 on 2016/8/12.
+ */
+public class MyGridView extends GridView {
+
+    public MyGridView(Context context) {
+        super(context);
+    }
+
+    public MyGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyGridView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(
+                Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
